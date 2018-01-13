ffc8fb8ca8582942ce919444b3fe951ea0553dd6
Skyoumans <skyyoumans@gmail.com>
2018-01-13 12:14:31 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.delete_all
+User.create([{ name: 'Skylar Youmans', email: 'skycodedev@gmail.com', password: "$KyC0De93", remember_created_at: nil }])
+User.create([{ name: 'James Smyntyna', email: 'jms.smyntyna@gmail.com', password: "change_me", remember_created_at: nil }])
